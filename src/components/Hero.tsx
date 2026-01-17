064eef29189eebb2a970a24eda95212911261b37
prakashpandey1508 <prakashpandey00002@gmail.com>
2026-01-17 23:52:26 +0530

    resume link update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -116,7 +116,7 @@ const Hero = () => {
         </div>
         {isOpen ? <Chatbot toggleChat={togglechat} /> : null}
         
-        <a href="" target="_blank" rel="noopener noreferrer" download>
+        <a href="https://drive.google.com/file/d/1YTtyL-0dfovwmwnMHuivnGsjvHYNDuVU/view?usp=sharing" target="_blank" rel="noopener noreferrer" download>
         <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-10 py-4 text-base font-medium text-white backdrop-blur-3xl">
